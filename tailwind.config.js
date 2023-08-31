e674ecdfa7862ed32746d969f7d4e07280a9449e
Andres Castro <andrescn20@gmail.com>
2023-08-30 20:40:08 -0600

    Extend theme: font and max widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      maxWidth: {
+        '8xl': '100rem',
+        '9xl': '120rem',
+      },
       backgroundImage: {
         'macbook': "url('/images/macbook.png')",
       },
@@ -10,6 +14,7 @@ export default {
         'inter': ['Inter', 'sans-serif'],
         'montserrat': ['Montserrat', 'sans-serif'],
         'poppins': ['Poppins', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
       },
       colors:{
         argold: '#B8A150',
