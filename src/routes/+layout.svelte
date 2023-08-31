3c78b3b7b095476fce9c93c725435bdf79c5bad4
Andres Castro <andrescn20@gmail.com>
2023-08-31 17:04:05 -0600

    Add Socials to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,14 @@
     import "../app.css";
     import Nav from "../components/nav.svelte";
     import Footer from "../components/footer.svelte";
+    import Socials from "../components/socials.svelte";
     let logo = "./images/logo.png";
 </script>
 
 <Nav logo={logo} />
 
+
 <slot></slot>
 
+<Socials />
 <Footer />
