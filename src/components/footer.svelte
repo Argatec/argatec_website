31463bd18ee5fbd6491bfefd39f8df7884853048
Andres Castro <andrescn20@gmail.com>
2023-08-30 20:37:20 -0600

    Footer component, still incomplete.

---
+++ src/components/footer.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class="bg-black text-argold">
+    <img src="./images/logo.png" alt="logo" class="w-32 mx-auto py-8">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div>    </div>
+
+</div>
