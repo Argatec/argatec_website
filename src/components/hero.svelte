746cff30df5f13bf4b8d451396431f39362152c9
Andres Castro <andrescn20@gmail.com>
2023-08-30 21:33:11 -0600

    Add Carousel to Hero

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -1,63 +1,38 @@
 <script lang="ts">
-  let macbook: string = "/images/macbook.png";
-  let camera: string = "/images/camera.png";
-  let router: string = "/images/router.png";
+  import { browser } from "$app/environment";
+  import Carousel from "svelte-carousel";
+  import Hero_Banner from "./hero_banner.svelte";
+
+
+  let carousel: any;
+
+  let banners: Array<Array<string>> = [
+    ['seguridad', "Seguridad", "Electrónica Integral"],
+    ['macbook', "Venta de Equipo", "Tecnológico"],
+    ['redes', "Soluciones en", "Tecnologia Informática"],
+  ];
+  let carousel_length: number = banners.length;
+
+  let currentIndex: number = 0;
+
+  const handleNextClick = () => {
+    currentIndex == carousel_length - 1 ? (currentIndex = 0) : currentIndex++;
+    carousel.goToNext();
+  };
+  const handlePreviousClick = () => {
+    currentIndex == 0 ? (currentIndex = carousel_length - 1) : currentIndex--;
+    carousel.goToPrev();
+  };
 </script>
 
-<div
-  class="hero z-20 px-12 bg-gradient-to-b/ from-black to-transparent text-white flex flex-col
-justify-center bg-macbook bg-bottom bg-no-repeat bg-cover h-[630px] items-center"
->
-  <div class="flex w-full justify-center max-w-9xl">
-    <button>
-      <svg
-        width="144"
-        height="144"
-        viewBox="0 0 144 144"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M84.0189 47.7444L54.221 71.5827L84.0189 95.421"
-          stroke="#FFFDFD"
-          stroke-width="7.92761"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </button>
-    <div class="grow px-12 relative top-10 ">
-      <p class="text-[76px] font-inter font-bold leading-normal">
-        Venta de Equipo <br /> Tecnológico
-      </p>
-      <a href="/"
-        ><p class="font-montserrat font-bold text-3xl py-4">Leer más</p></a
-      >
-    </div>
-    <button>
-      <svg
-        width="144"
-        height="144"
-        viewBox="0 0 144 144"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M59.6124 47.7444L89.4103 71.5827L59.6124 95.421"
-          stroke="#FFFDFD"
-          stroke-width="7.92761"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </button>
-  </div>
-</div>
 
-<style>
-  .hero{
-    box-shadow: inset 0px 3em 3em #000;
-  }
-</style>
+  {#if browser}
+    <Carousel bind:this={carousel} arrows={false} dots={false}>
+      {#each banners as banner}
+        <Hero_Banner imagen={banner[0]} handleNextClick={handleNextClick}
+        handlePreviousClick={handlePreviousClick} texto_arriba={banner[1]} texto_abajo={banner[2]}/>
+      {/each}
+    </Carousel>
+  {/if}
+
+
