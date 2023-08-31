00b65cae35272fdb7e3c3d81cb3e174bdd675c63
Andres Castro <andrescn20@gmail.com>
2023-08-31 15:35:11 -0600

    Correct nav bar sizes

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -2,9 +2,9 @@
   export let logo: string;
 </script>
 
-<nav class=" text-2xl bg-black text-white z-20 font-bold flex justify-center">
+<nav class=" text-xl bg-black text-white z-20 font-bold flex justify-center">
   <div class="flex items-center grow max-w-9xl">
-    <img src={logo} alt="Argatec" class="w-48" />
+    <img src={logo} alt="Argatec" class="w-36" />
     <div class="space-x-12 pr-24 flex items-center py-16 grow justify-end">
       <a class="" href="/">Inicio</a>
       <a href="/seguridad">Seguridad</a>
