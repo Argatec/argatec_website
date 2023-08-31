efd6e7291e1f23d2128d7bd5b547a6d81a5741bf
Andres Castro <andrescn20@gmail.com>
2023-08-30 20:38:31 -0600

    Extract navbar from layout as component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,12 @@
 <script>
     import "../app.css";
+    import Nav from "../components/nav.svelte";
+    import Footer from "../components/footer.svelte";
     let logo = "./images/logo.png";
 </script>
-<nav class="text-2xl flex justify-end bg-black text-white z-20 font-bold">  
-  <div class="flex grow items-center justify-center">
-    <img src={logo} alt='Argatec' class="w-48" />
-  </div>
-   <div class="space-x-12 pr-24 flex items-center py-16">
-      <a class="" href="/">Inicio</a>
-      <a href="/seguridad">Seguridad</a>
-      <a href="/soluciones_tecnologicas">Soluciones Tecnológicas</a>
-      <a href="/equipo_informatico">Equipo Informático</a>
-    </div>
-</nav>
+
+<Nav logo={logo} />
 
 <slot></slot>
+
+<Footer />
