8bb4889928c1ba2ae3e9be0781c693635a149b06
Andres Castro <andrescn20@gmail.com>
2024-01-21 20:40:29 -0600

    Add Facebook link to store.

--- src/lib/links.js
+++ src/lib/links.js
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 
 // Default WhatsApp link
 export const whatsapp_link = writable('https://wa.me/72211112');
+
+export const facebook_link = writable('https://www.facebook.com/Argatec.sa')
