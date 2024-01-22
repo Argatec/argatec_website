8450888da9d54318437ed845dd0a105d3b832c20
Andres Castro <andrescn20@gmail.com>
2024-01-21 20:40:53 -0600

    Part of Hero refactoring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 
 
-  import Hero from "../components/hero.svelte";
+  import Hero from "../components/hero_multiple.svelte";
   import Contacto from "../components/contacto.svelte";
   import Clientes from "../components/clientes.svelte";
   import Servicios from "../components/servicios.svelte";
