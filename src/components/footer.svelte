afa9dbaf7fca53c9ea4c6345a01c5b0b2a64a8c1
Andres Castro <andrescn20@gmail.com>
2024-01-21 20:39:15 -0600

    Create Footer Layout

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,12 +1,76 @@
 <script>
-
+  import { whatsapp_link } from "$lib/links";
+  import { facebook_link } from "$lib/links";
 </script>
 
-<div class="bg-black text-argold">
-    <img src="./images/logo.png" alt="logo" class="w-32 mx-auto py-8">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div>    </div>
-
+<div class="bg-black text-white flex flex-col items-center px-10 py-12">
+  <p class="font-poppins text-4xl w-2/5 text-center">¡lorem ipsum amelia astrid cmajs oasinoin!</p>
+  <div class="text-sm my-12 font-poppins space-x-4 w-full flex justify-center">
+    <a
+      class="flex justify-center items-center py-3 w-44 bg-transparent text-argold border-2 border-argold rounded-md"
+      href={$whatsapp_link}
+      target="_blank">Whatsapp</a
+    >
+    <a
+      class="flex justify-center items-center py-3 w-44 bg-argold text-black rounded-md"
+      href={$facebook_link}
+      target="_blank">Facebook</a
+    >
+  </div>
+  <div class="my-4 h-0.5 w-full bg-argold"></div>
+  <div class="text-argold my-8">© 2023. All rights reserved.</div>
+  <div class="flex w-full h-6">
+    <div class="grow flex space-x-4 pl-12">
+      <div>
+        <svg
+          height="22"
+          style="fill:#B8A150;fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+          version="1.1"
+          viewBox="0 0 512 512"
+          width="100%"
+          xml:space="preserve"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          ><path
+            d="M512,257.555c0,-141.385 -114.615,-256 -256,-256c-141.385,0 -256,114.615 -256,256c0,127.777 93.616,233.685 216,252.89l0,-178.89l-65,0l0,-74l65,0l0,-56.4c0,-64.16 38.219,-99.6 96.695,-99.6c28.009,0 57.305,5 57.305,5l0,63l-32.281,0c-31.801,0 -41.719,19.733 -41.719,39.978l0,48.022l71,0l-11.35,74l-59.65,0l0,178.89c122.385,-19.205 216,-125.113 216,-252.89Z"
+            style="fill-rule:nonzero;"
+          /></svg
+        >
+      </div>
+      <div>
+        <svg
+        height="22"
+        style="fill:#B8A150"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 56.7 56.7"
+          enable-background="new 0 0 56.7 56.7"
+          xml:space="preserve"
+        >
+          <g>
+            <path
+              d="M28.2,16.7c-7,0-12.8,5.7-12.8,12.8s5.7,12.8,12.8,12.8S41,36.5,41,29.5S35.2,16.7,28.2,16.7z M28.2,37.7
+                    c-4.5,0-8.2-3.7-8.2-8.2s3.7-8.2,8.2-8.2s8.2,3.7,8.2,8.2S32.7,37.7,28.2,37.7z"
+            />
+            <circle cx="41.5" cy="16.4" r="2.9" />
+            <path
+              d="M49,8.9c-2.6-2.7-6.3-4.1-10.5-4.1H17.9c-8.7,0-14.5,5.8-14.5,14.5v20.5c0,4.3,1.4,8,4.2,10.7c2.7,2.6,6.3,3.9,10.4,3.9
+                    h20.4c4.3,0,7.9-1.4,10.5-3.9c2.7-2.6,4.1-6.3,4.1-10.6V19.3C53,15.1,51.6,11.5,49,8.9z M48.6,39.9c0,3.1-1.1,5.6-2.9,7.3
+                    s-4.3,2.6-7.3,2.6H18c-3,0-5.5-0.9-7.3-2.6C8.9,45.4,8,42.9,8,39.8V19.3c0-3,0.9-5.5,2.7-7.3c1.7-1.7,4.3-2.6,7.3-2.6h20.6
+                    c3,0,5.5,0.9,7.3,2.7c1.7,1.8,2.7,4.3,2.7,7.2V39.9L48.6,39.9z"
+            />
+          </g>
+        </svg>
+      </div>
+    </div>
+    <div class="text-argold font-inter text-base space-x-4">
+      <a href="/">Inicio</a><a href="/seguridad">Seguridad Electrónica</a><a href="/soluciones_tecnologicas"
+        >Soluciones Tecnologicas</a
+      ><a href="equipo_informatico">Equipo Informatico</a>
+    </div>
+  </div>
 </div>
