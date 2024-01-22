76db6be3d7543aa9b5ca246fa7b2238e16d07f54
Andres Castro <andrescn20@gmail.com>
2024-01-21 20:39:42 -0600

    Refactor Hero for Single and Multiple Images

---
+++ src/components/hero_simple.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+
+  import Simple_Banner from "./simple_banner.svelte";
+
+  let banner: Array<string> = ["soluciones_tecnologicas", "Soluciones en Tecnología", "Informática", "TEXTO PENDIENTE PERO ES ALGO AHI LARGO QUE DESCRIBE UNAS WEAS MISTICAS DE LAS SOLUCIONES EN TECNOLOGIA"];
+</script>
+
+{#if browser}
+  <Simple_Banner imagen={banner[0]} titulo_arriba={banner[1]} titulo_abajo={banner[2]} texto_inferior={banner[3]}/>
+{/if}
