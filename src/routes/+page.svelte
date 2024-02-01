6190247f9cb55e487ed1e5b37ec43335dc5ead0a
Andres Castro <andrescn20@gmail.com>
2024-01-31 22:07:16 -0600

    Actualizar Nombres de Servicios Home Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import Hero from "../components/hero_multiple.svelte";
   import Contacto from "../components/contacto.svelte";
   import Clientes from "../components/clientes.svelte";
-  import Servicios from "../components/servicios.svelte";
+  import Servicios from "../components/servicios_principales.svelte";
   
   
 </script>
