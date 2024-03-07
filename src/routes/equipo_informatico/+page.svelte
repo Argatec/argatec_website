118acee2481e9a4ed48d70813e7a8a4fd1f6dc2b
Andres Castro <andrescn20@gmail.com>
2024-03-07 11:53:58 -0600

    Remove yet another instance of the text

--- src/routes/equipo_informatico/+page.svelte
+++ src/routes/equipo_informatico/+page.svelte
@@ -9,6 +9,6 @@
     imagen= "soluciones_tecnologicas"
     titulo_arriba = "Soluciones en Tecnología"
     titulo_abajo = "Informática"
-    descripcion = "TEXTO PENDIENTE PERO ES ALGO AHI LARGO QUE DESCRIBE UNAS WEAS MISTICAS DE LAS SOLUCIONES EN TECNOLOGIA"/>
+    descripcion = ""/>
     
     
