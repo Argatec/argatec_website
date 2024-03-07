8c278faaaeffee9f4ce77cb2a5e8a3d6b8623a41
Andres Castro <andrescn20@gmail.com>
2024-03-07 11:47:56 -0600

    Remove Seguridad from Nav

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -7,7 +7,7 @@
     <img src={logo} alt="Argatec" class="w-32 my-3" />
     <div class="space-x-12 pr-24 flex items-center grow justify-end">
       <a class="" href="/">Inicio</a>
-      <a href="/seguridad">Seguridad</a>
+      <!-- <a href="/seguridad">Seguridad</a> -->
       <a href="/soluciones_tecnologicas">Soluciones Tecnológicas</a>
       <!-- <a href="/equipo_informatico">Equipo Informático</a> -->
     </div>
